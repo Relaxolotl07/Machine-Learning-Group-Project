42ab5ec7882b994c2298181e3772d1ff270479c8
Hunter <HunterHoinkes@my.unt.edu>
2025-04-07 19:05:15 -0500

    Data and validation reformatted to python, created Model Architecture notebook for rapid testing
    title. Added comments

---
+++ Data_and_Validation.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# The paper provided describes a neural network architechture focused around efficient parallel sorting.
+
+# This file inludes randomly generated sample data for our model to sort and a validation function to compare the sorted results.
+
+
+# n = number of elements in the list
+# low & high = range of random integers
+def create_data(n, low=1, high=101):
+    data = np.random.randint(low, high, n).tolist()
+    return data
+
+
+# simple linear confirmation for validating the sorted data
+# ascending = True for ascending order, False for descending order
+def validate_data(data, ascending=True):
+    for i in range(len(data) - 1):
+        if ascending:
+            if data[i] > data[i + 1]:
+                return False
+        else:
+            if data[i] < data[i + 1]:
+                return False
+    return True
