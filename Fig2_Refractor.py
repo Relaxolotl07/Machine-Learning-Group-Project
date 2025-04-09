b61f42ce829e5f6f07b6df7c5cc057c714b79b78
Alexander Evans <alexanderevans2@my.unt.edu>
2025-04-09 14:43:17 -0500

    Added file structure, Main.py and Fig2_Refractor.py

---
+++ Fig2_Refractor.py
@@ -0,0 +1,19 @@
+# Refractor of Fig2.m into python
+# April 9 2024
+# Alexander Evans
+
+#
+# Started Refractor
+# To run call in main.py
+#
+#
+#
+#
+#
+
+
+
+
+def fig2_refractor () :
+    print("Hello World")
+    return
---
+++ Main.py
@@ -0,0 +1,6 @@
+#Driver for project, import and call functions here
+
+from Data_and_Validation import create_data, validate_data
+from Fig2_Refractor import fig2_refractor
+
+fig2_refractor()
