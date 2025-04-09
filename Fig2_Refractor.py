393c235fdf24ba3f61a127a66519d710094ea704
Alexander Evans <alexanderevans2@my.unt.edu>
2025-04-09 14:50:52 -0500

    Data randomization for fig2

--- Fig2_Refractor.py
+++ Fig2_Refractor.py
@@ -6,7 +6,7 @@
 # Started Refractor
 # To run call in main.py
 #
-#
+# Random data initialization done
 #
 #
 #
@@ -15,5 +15,25 @@
 
 
 def fig2_refractor () :
-    print("Hello World")
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import time
+
+    # Paramter Initialization
+    # Trajectories
+    n=9
+    # Iterations
+    j=10
+
+    # dmin: tracks smallest value of k in d array
+    # dmax: largest k values in d array
+    # tmax: max time to execute the inner simulation loop for k
+    dmin, dmax, tmax = 0, 0, 0
+
+    # Random Data Generation
+    # Generate array of random numbers
+    a = np.random.rand(n)
+    # scale to [-1, 1]
+    sa = (a - 0.5) * 2
+    
     return
