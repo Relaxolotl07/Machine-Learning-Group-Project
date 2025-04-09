4c32fcef4e6c8da6dd7f4cd203ddb7590801971f
Alexander Evans <alexanderevans2@my.unt.edu>
2025-04-09 15:42:46 -0500

    Done with plotting, a little rough and could use optimization with numpy arrays and operations.

--- Fig2_Refractor.py
+++ Fig2_Refractor.py
@@ -36,8 +36,6 @@ def fig2_refractor (sa) :
     # Normalize sa into array a
     a = sa/10 + 0.5
 
-    print (a)
-
     #initialize ki and aa
     ki = []
     # aa is for plotting
@@ -48,11 +46,40 @@ def fig2_refractor (sa) :
     for l in range(n):
         aa.append([])
 
-    # plotting 
+    # plotting preparation
     for x in range(j):
         ki.append(x)
         for l in range(n):
             aa[l].append(sa[l])
+    # convert aa into numpy array for slicing operation later
+    aa = np.array(aa)
 
+    # Subplot 1 trajectories
+    # Define the subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,8))
+    # define plot styles
+    plot_styles = ['ko-', 'rh-', 'b<-', 'm^-', 'gv-', 'kp-', 'cd-', 'rx-', 'b*-']
+    # set the label for the subplots
+    labels = [f'a_{i+1}' for i in range(n)] # f is for shorthand formatting in python
+    # Set each subplot with the plotstyle and label according to fig2
+    for i in range(n):
+        ax1.plot(ki, aa[i, :], plot_styles[i], label=labels[i])
+
+    # x and y limit for ax1
+    ax1.set_xlim(1,j)
+    ax1.set_ylim(-5, 5)
+    # x and y labels
+    ax1.set_xlabel('Number of iteration l')
+    ax1.set_ylabel('Trajectories a_1 to a_9')
+    # activate the legend
+    ax1.legend()
+    # set to show the grid
+    ax1.grid(True)
+   
+    
+    
+    
+    
+    
     
     return
