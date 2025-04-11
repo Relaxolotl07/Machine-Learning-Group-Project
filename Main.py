24093dcc8834a0d6f4509611cfd2870b6e607e0d
Alexander Evans <alexanderevans2@my.unt.edu>
2025-04-10 20:08:11 -0500

    Fixed an issue where only d_1 would be calculated correctly. Should properly reflect the graph as well as multiple random arrays.

--- Main.py
+++ Main.py
@@ -11,6 +11,23 @@ import numpy as np
 from Data_and_Validation import create_data, validate_data
 from Fig2_Refractor import fig2_refractor
 
+n=9
+low  =-5
+high = 5
 # static variables for testing, feel free to randomize variables as they are normalized within fig2
 sa = np.array([-0.8805, 2.4457, -2.3205, -0.6008, 4.3338, 1.8333, -2.8744, 3.3924, 1.2878])
+sa1 = create_data(n, low, high)
+sa2 = create_data(n, low, high)
+sa3 = create_data(n, low, high)
+sa4 = create_data(n, low, high)
+
+print("Fixed Data: \n")
 fig2_refractor(sa)
+print("Random 1 Data: \n")
+fig2_refractor(sa1)
+print("Random 2 Data: \n")
+fig2_refractor(sa2)
+print("Random 3 Data: \n")
+fig2_refractor(sa3)
+print("Random 4 Data: \n")
+fig2_refractor(sa4)
